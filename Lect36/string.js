5ed8b9dd323ad912b5b74635d220b89647917624
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-08-03 22:07:02 +0530

    string

---
+++ Lect36/string.js
@@ -0,0 +1,17 @@
+/* const str = "Shriyansh Rajpoot";
+const str2 = 'Shriyansh Rajpoot';
+const str3 = `intel i5 12th gen 12450hx`;
+console.log(str,str2,str3); */
+
+// string length
+/* const str3 = `intel i5 12th gen 12450hx`;
+console.log(str3.length); */
+
+// print string character
+const str3 = `intel i5 12th gen 12450hx`;
+/*console.log(str3[3]); */
+
+// convert string to capital letter or uppercase
+/* console.log(str3.toUpperCase()); */
+
+ 
