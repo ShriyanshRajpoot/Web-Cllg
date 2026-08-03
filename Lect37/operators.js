2572d8cb8c025a08afbb29de401eafcff78f2ece
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-08-03 22:07:27 +0530

    operators

---
+++ Lect37/operators.js
@@ -0,0 +1,32 @@
+// Assignment Operator
+
+/* let x = 20; 
+let y = 10;
+x+=y;
+x-=y;
+console.log(x); */
+
+
+// Comparison Operator
+
+/* let x = 20;
+let y = 10;
+console.log(x>y);
+console.log(x<y);
+console.log(x==y); (it convert string to number then compare)
+console.log(x===y); (directly compare) */
+
+
+// Convert String to Number
+
+/* let z = "120";
+let a = Number(z);
+console.log(typeof a); */
+
+
+// NAN (Not A Number)
+
+/* let a = "121ac";
+let b = Number(a);
+console.log(typeof b); */
+
