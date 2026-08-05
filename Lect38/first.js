340a2e76da77a68e769ce013a54fa57d352d4eb0
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-08-05 22:47:36 +0530

    crud

---
+++ Lect38/first.js
@@ -0,0 +1,22 @@
+// create
+/* const newElement = document.createElement("h2");
+newElement.textContent = "Strike is Coming";
+newElement.id = "second";
+
+// select
+const element = document.getElementById("first");
+element.after(newElement); 
+
+const newElement2 = document.createElement("h2");
+newElement2.textContent = "Strike is Coming";
+newElement2.id = "second";
+
+// give class
+newElement2.className = "forza";
+
+// add/remove class
+newElement2.classList.add("horizon");
+newElement2.classList.remove("horizon");
+
+newElement2.style.backgroundColor = "brown";
+newElement2.style.fontSize = "30px"; */
