f7d1dbab0d8d34067c9bb3fd8096d0c945721b7c
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2026-09-08 22:20:10 +0530

    p1

---
+++ Lect59/index.jsx
@@ -0,0 +1,17 @@
+import Card from './components/Card'
+
+function App() {
+ 
+  return (
+    <div>
+       <Card name='love babbar'>
+        <h1>Best Web Dev Course</h1>
+        <p>Trying to be consistent in this</p>
+        <p>Will Complete the course soon </p>
+
+       </Card>
+    </div>
+  )
+}
+
+export default App
